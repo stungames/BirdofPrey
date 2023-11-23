b24c3025b957e2c625cec7542c0d826747a98d57
De1readh <121015959+De1readh@users.noreply.github.com>
2023-11-23 15:56:00 +0300

    assignment

--- Source/BirdOfPrey/Private/BaseGameAgent.h
+++ Source/BirdOfPrey/Private/BaseGameAgent.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "BaseGameInstance.h"
+#include "BaseWeapon.h"
+#include "Sound/SoundCue.h"
 #include "BaseGameAgent.generated.h"
+
 
 UCLASS()
 class ABaseGameAgent : public APawn
@@ -15,6 +19,50 @@ public:
 	// Sets default values for this pawn's properties
 	ABaseGameAgent();
 
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey|Effects")
+	UParticleSystem* DeathParticleEffect;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey|Effects")
+	USoundCue* DeathSoundCue;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey|Effects")
+	USoundCue* HitSoundCue;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey|Effects")
+	UParticleSystem* HitParticleEffect;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	float MoveSpeed;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	ABaseWeapon* Weapon;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	TSubclassOf<ABaseWeapon> DefaultWeaponType;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	float Health;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	float Points;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	bool bCheckForOutOfBounds;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	FName WeaponSocketName;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	float OutOfBoundsCheckTolerance;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	EAttachmentRule WeaponAttachmentRule; 
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	FVector TargetLocation; 
+
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -22,6 +70,55 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	TSubclassOf<ABaseWeapon> ChangeWeaponType();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "BirdOfPrey")
+	void GetWeaponSpawnTransform(FTransform& SpawnTransform);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void SpawnDefaultWeapon();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void StartFire();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void StopFire();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	virtual void TakeDamage(float Damage, float& ActualDamage);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "BirdOfPrey")
+	void IsAlive(bool& Alive);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void Died(AController* Killer);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void PlayHitEffects();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void CleanUp();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void ApplyAgentInfo(FSAgentInfo NewAgentInfo);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void AimAt(FVector AimTarget);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void PlayDeathEffects();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void CheckForOutOfBounds();
+
+
+
+
+
+
+
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
---
+++ Source/BirdOfPrey/Private/BaseGameMode.h
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BasePlayerController.h"
+#include "BaseGameAgent.h"
+#include "BasePowerUp.h"
+#include "Engine/DataTable.h"
+#include "WorldCamera.h"
+#include "BaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	float WorldScrollSpeed;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	FVector2D MaxRelativePlayerOffset;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	AWorldCamera* WorldCamerAActor;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	UDataTable* PlayerAgentInfoTable;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	float RespawnDelay;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	float SpawnOffset;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	FTimerHandle StationarySpawnTimer;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	FTimerHandle ShipSpawnTimer;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	TArray<TSubclassOf<ABasePowerUp>> PowerUpList;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	float PickUpSpawnPercent;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BirdOfPrey")
+	bool isGameOverScreen;
+
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	FVector GetWorldScrollVelocity();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void GetWorldCameraActor(AWorldCamera* WorldCameraAActor);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void OnPlayerDied(ABasePlayerController* Player);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void OnEnemyDied(ABaseGameAgent* Enemy, AController* Killer);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void StartGame();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void EndGame(bool Success);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void SpawnEnemyFrom(ABaseGameAgent* ClassList);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void ResetGame();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void RespawnPlayer();
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void TrySpawnPowerUp(FVector Location);
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	float GetDistanceTravelled();
+};
